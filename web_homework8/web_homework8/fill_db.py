823c4b649bcfd4b61b06e82d6f685b2e1007a8f4
Taras <93609222+aps06@users.noreply.github.com>
2025-07-13 06:16:11 +0200

    Update fill_db.py

--- web_homework8/web_homework8/fill_db.py
+++ web_homework8/web_homework8/fill_db.py
@@ -9,17 +9,17 @@ connect(
 )
 
 
-with open("data/authors.json", "r", encoding="utf-8") as f:
+with open("data\\authors.json", "r", encoding="utf-8") as f:
     authors_list = json.load(f)
 for item in authors_list:
     author = Authors(
-        fullname=item.get("fullname"),
+        fullname=item.get("fullname").replace("-", " "),
         born_date=item.get("born_date"),
         born_location=item.get("born_location"),
         description=item.get("description"),
     )
     author.save()
-with open("data/quotes.json", "r", encoding="utf-8") as f:
+with open("data\\quotes.json", "r", encoding="utf-8") as f:
     quotes_list = json.load(f)
 for item in quotes_list:
     author_name = item.get("author")
